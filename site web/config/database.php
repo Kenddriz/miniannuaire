4008600cc70048437086b95c4fe7149cec320ff4
Kenddriz <rcarandrcharlin@gmail.com>
2020-06-16 15:03:23 +0300

    Human CartOffice avec config local

--- site web/config/database.php
+++ site web/config/database.php
@@ -2,10 +2,10 @@
    
 <?php 
 return array(
-    "user" => "epiz_26016766",
-    "password" => "bgCca3sgvr",
-    "db" => "epiz_26016766_cartoffice",
-    "host" => "sql107.epizy.com"
+    "user" => "root",
+    "password" => "",
+    "db" => "cartoffice",
+    "host" => "localhost"
 );
 ?>
   
